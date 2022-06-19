56140321f182a3e2f522a11f368433786870f692
nzagaspard <65093165+nzagaspard@users.noreply.github.com>
2022-06-19 21:41:31 +0200

    Added Home

---
+++ home.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+def home():    
+    title = """<div style="background-color:#02ab21;padding:1px">
+                <h3 style="color:white;text-align:center;">Business Metrics (Explanation, Calculation, Intepretation)</h3></div>"""
+
+    st.markdown(title, unsafe_allow_html=True)
+    
+    st.markdown("""<br><h5 style="text-align:justify;">A one stop center application for business metrics insights and calculators. Even though it is still in development some of its applications are live. It will feature metrics such as Net Promoter Score, Churn Rate, Cohort Analytics, Customers Loyalty & Retention among others.</h5><br> 👈 On the left you can navigate to a desired app.""", unsafe_allow_html=True)
+    
+    st.markdown("#### Contact/Connect/Follow me")
+    
+    c1, c2, c3, c4, c5, c6 = st.columns(6)
+       
+    c1.markdown("""[<img align="left" alt="linked-in" src="https://img.shields.io/badge/linkedin-%230077B5.svg?&style=for-the-badge&logo=linkedin&logoColor=white" />](https://www.linkedin.com/in/gaspard-nzasabimfura/)""", unsafe_allow_html=True)
+    
+#     c2.markdown("""[<img align="left" alt="facebook" src="https://img.shields.io/badge/facebook-%231877F2.svg?&style=for-the-badge&logo=facebook&logoColor=white" />](https://www.facebook.com/nzasabimana.gaspard/)""", unsafe_allow_html=True)
+    
+    c2.markdown("""[<img align="left" alt="twitter" src="https://img.shields.io/badge/twitter-%231DA1F2.svg?&style=for-the-badge&logo=twitter&logoColor=white" />](https://twitter.com/nzagaspard)""", unsafe_allow_html=True)
+
+    c3.markdown("""[<img align="left" alt="telegram" src="https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white" />](https://t.me/nzagaspard)""", unsafe_allow_html=True)
+
+    c4.markdown("""[<img align="left" alt="whatsapp" src="https://img.shields.io/badge/WhatsApp-25D366?style=for-the-badge&logo=whatsapp&logoColor=white" />](https://wa.me/250722882193)""", unsafe_allow_html=True)
+    
+    c5.markdown("""[<img align="left" alt="linked-in" src="https://img.shields.io/badge/website-000000?style=for-the-badge&logo=About.me&logoColor=white" />](https://nzagaspard.github.io/)""", unsafe_allow_html=True)
+    
+    c6.markdown("""[<img align="left" alt="linked-in" src="https://img.shields.io/badge/GitHub-100000?style=for-the-badge&logo=github&logoColor=white"  />](https://github.com/nzagaspard)""", unsafe_allow_html=True)
--- master_app.py
+++ master_app.py
@@ -3,6 +3,7 @@ from streamlit_option_menu import option_menu
 from nps_calculator import nps
 from customer_loyality_retention import clr
 from churn_rate import churn_rate
+from home import home
 
 st.set_page_config(page_title="Business Metrics",page_icon=":bar_chart:")
 
@@ -18,11 +19,11 @@ st.markdown(hide_st_style, unsafe_allow_html=True)
 # st.sidebar.title('Applications')
 # apps = st.sidebar.selectbox('Choose the application', ['Net Promoter Score Calculator', 'Customer Loyality Retention', 
 #                                                        'Churn Rate Analysis & Prediction'])
-app_choices = ['Net Promoter Score', 'Customer Loyality Retention', 'In Pipeline']
+app_choices = ['Home','Net Promoter Score', 'Customer Loyality Retention', 'In Pipeline']
 
 with st.sidebar:
     apps = option_menu("App Gallery", app_choices,
-                         icons=['bookmark-heart', 'sort-numeric-down', 'bar-chart-steps'],
+                         icons=['house','bookmark-heart', 'sort-numeric-down', 'bar-chart-steps'],
                          menu_icon="app-indicator", default_index=0,
                          styles={
         "container": {"padding": "5!important", "background-color": "#fafafa"},
@@ -32,8 +33,10 @@ with st.sidebar:
     }
     )
 if apps == app_choices[0]:
-    nps()
+    home()
 elif apps == app_choices[1]:
-    clr()
+    nps()
 elif apps == app_choices[2]:
+    clr()
+elif apps == app_choices[3]:
     churn_rate()
